269a7eff414f06958df86565d908881feda1737d
CSant04y <esquivelcarlos27893@gmail.com>
2021-10-18 19:19:53 +0000

    fixed whitspace

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-a type-annotated function sum_mixed_list which takes a 
+a type-annotated function sum_mixed_list which takes a
 list mxd_lst of integers and floats and returns their sum as a float
 """
 

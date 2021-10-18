0efa3b465b6b53710468808c23fe9f4f16fcbe6f
CSant04y <esquivelcarlos27893@gmail.com>
2021-10-18 16:25:31 +0000

    added task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env pythnon3
+"""
+type-annotated function floor which takes a float n as argument
+ and returns the floor of the float.
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    '''Returns the floor value of float passed'''
+    return math.floor(n)

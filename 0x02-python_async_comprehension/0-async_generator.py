ac5b06033a201dfa329bf86552cd66ee8f0be391
CSant04y <esquivelcarlos27893@gmail.com>
2021-10-20 19:13:30 +0000

    added function annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,9 +5,10 @@
 
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None]:
     """[This is an async generator that loops ten times]
     """
     for i in range(10):

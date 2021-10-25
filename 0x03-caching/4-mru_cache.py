47d1737fff6ba3fa8dd8a72c7b7f2f2494f33622
CSant04y <esquivelcarlos27893@gmail.com>
2021-10-25 16:26:29 +0000

    added lat task

---
+++ 0x03-caching/4-mru_cache.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""[Least Recently Used]
+"""
+
+
+from collections import OrderedDict
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """[This is the Least Recently Used Caching class]
+
+    Args:
+        BaseCaching ([Parent Class]): [This is the class that LRUcache]
+    """
+
+    def __init__(self) -> None:
+        """[Initilization using parent init]
+        """
+        super().__init__()
+        self.mru_dict = OrderedDict()
+
+    def put(self, key, item):
+        """[This assigns the dictonary]
+        Args:
+            key ([str]): [key]
+            item ([str]): [Value]
+        """
+        if not key and item:
+            return
+
+        self.mru_dict[key] = item
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded = next(iter(self.mru_dict))
+            del self.cache_data[discarded]
+            print("DISCARD: {}".format(discarded))
+
+        self.mru_dict.move_to_end(key, last=False)
+
+    def get(self, key):
+        """[Returns value based of key]
+
+        Args:
+            key ([str]): [key]
+        """
+        if key in self.cache_data:
+            self.mru_dict.move_to_end(key, False)
+            return self.cache_data[key]
+
+        return None

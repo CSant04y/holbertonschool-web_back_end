f2eac21f7622fce5622ee2d5af7a08d45d4b40a7
CSant04y <esquivelcarlos27893@gmail.com>
2021-10-31 16:42:43 +0000

    added tsk 1

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This function returns a long message Obfiscated
+"""
+
+
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """[Filter_datum takes in a list of strings
+        that have PII That needs to be obfiscated]
+    """
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',
+                             f'{field}={redaction}{separator}', message)
+    return message

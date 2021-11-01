7107097b5b0b4d2d1ffd9791e20b3b01d794b852
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-01 19:12:15 +0000

    added file

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -16,3 +16,16 @@ def hash_password(password: str) -> bytes:
         bytes: [return]
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """[validates hashed passwd]
+
+    Args:
+        hashed_password (bytes): [description]
+        password (str): [description]
+
+    Returns:
+        bool: [description]
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

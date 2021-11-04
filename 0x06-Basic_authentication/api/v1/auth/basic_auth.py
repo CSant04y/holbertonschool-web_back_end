1528fb5377c033312745bdae20b5c3ecde491eba
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-04 14:36:59 +0000

    added task 6

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,9 +3,26 @@
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Class BasicAuth that inherits from Auth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """[extracts a base64 encoded str from str]
+
+        Args:
+            authorization_header (str): [Header authorization for API]
+
+        Returns:
+            str: [base64 encoded str]
+        """
+        if not authorization_header or not isinstance(authorization_header,
+                                                      str):
+            return None
+
+        if authorization_header.startswith("Basic "):
+            return authorization_header[6:]
+        # base64.b64encode(authorization_header[6:].encode("ascii"))

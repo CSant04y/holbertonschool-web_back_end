bcbf212e887f8501ea0bc196216fb6ba32ae2f99
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-04 13:53:19 +0000

    Added conditonal statment for env var

---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" This Class inherits from BasicAuth
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Class BasicAuth that inherits from Auth
+    """
+    pass

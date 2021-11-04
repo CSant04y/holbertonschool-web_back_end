3f7db89affe61c93e792b2ef7038f52b8a4f1655
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-04 01:20:08 +0000

    added task

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -8,13 +8,9 @@ from typing import List, TypeVar
 class Auth():
     """Class Auth
     """
-    def __init__(self):
-        """ class constructor
-        """
-        pass
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """This takes care of the path anf returns a bool
+        """This takes care of the path and returns a bool
         """
         if not path or not excluded_paths:
             return True
@@ -33,9 +29,12 @@ class Auth():
     def authorization_header(self, request=None) -> str:
         """This is Authorization header
         """
-        return request
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns flask Request object
         """
-        return request
+        return None

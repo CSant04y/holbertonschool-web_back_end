6aef0893173691f3729767ce3868bbbc3eb2f39f
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-10 16:47:35 +0000

    Added class with inheritance

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Class SessionAuth that inherits from Auth
+"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """[This class is the basis of Session authentication]
+
+    Args:
+        Auth ([Class]): [Class that is being inhertited]
+    """
+    pass

2783305bfa2157196c5ea32f602abe5d9c3f5e8e
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-10 17:01:13 +0000

    addedd session auth class method

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@
 
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
@@ -12,4 +13,20 @@ class SessionAuth(Auth):
     Args:
         Auth ([Class]): [Class that is being inhertited]
     """
-    pass
+    user_id_by_session_id = dict()
+
+    def create_session(self, user_id: str = None) -> str:
+        """[This method creates a session for authentication]
+
+        Args:
+            user_id (str): [This is the Session ID]. Defaults to None.
+
+        Returns:
+            str: [description]
+        """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        
+        else:
+            self.user_id_by_session_id[str(uuid4())] = user_id
+            return self.user_id_by_session_id

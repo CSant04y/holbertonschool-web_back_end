02dbcb5d474d7a4160f5846a0530630e5febffcf
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-10 19:44:53 +0000

    added task

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -53,10 +53,11 @@ def request_validation():
     """ Validates request
     """
     path_list = ['/api/v1/status/', '/api/v1/unauthorized/',
-                 '/api/v1/forbidden/']
+                 '/api/v1/forbidden/', '/api/v1/auth_session/login/']
 
     if auth and auth.require_auth(request.path, path_list):
-        if not auth.authorization_header(request):
+        if not auth.authorization_header(request) and not auth.session_cookie(
+                request):
             abort(401)
         if not auth.current_user(request):
             abort(403)

f32933b6474cd7553e3ccb9762e57929be6b8a94
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-15 17:23:40 +0000

    added basic Flask application to app.py module

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env pyhton3
+"""This is another basic Flask Application
+"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome():
+    '''Returns Json Payload'''
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == '__main__':
+    app.run()

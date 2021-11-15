a9cc6f0abb923b5801a465d5bbeb158039f29b44
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-15 17:52:56 +0000

    added endpoint to flask application.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """This is another basic Flask Application
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from werkzeug.exceptions import NotFound
+from auth import Auth
 
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -11,6 +15,19 @@ app = Flask(__name__)
 def welcome():
     '''Returns Json Payload'''
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'])
+def register_user():
+    '''This method registers the user using a POST request'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':

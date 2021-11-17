0c957fddcc44e279ca8898c74ac7cc9ab23b252a
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-17 20:00:38 +0000

    added user login

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """This is another basic Flask Application
 """
-from flask import Flask, jsonify, request
-from werkzeug.exceptions import NotFound
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -18,7 +17,7 @@ def welcome():
 
 
 @app.route('/users', methods=['POST'])
-def register_user():
+def register_user() -> str:
     '''This method registers the user using a POST request'''
     email = request.form.get('email')
     password = request.form.get('password')
@@ -28,6 +27,28 @@ def register_user():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    """
+    This is a login function
+    """
+    email = request.form.get('email')
+    password = request.form.get('pasword')
+
+    valid_login = AUTH.valid_login(email, password)
+
+    if valid_login:
+        session_id = AUTH.create_session(email)
+        message = {"email": "<user email>", "message": "logged in"}
+        response = jsonify(message)
+        response.set_cookie('session_id', session_id)
+
+        return response
+
+    else:
+        abort(401)
 
 
 if __name__ == '__main__':

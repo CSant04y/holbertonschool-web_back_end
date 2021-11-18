e12fa3e0296924a26ff91ac18ae09876a9bcb7b3
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-18 01:13:36 +0000

    added chnages

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -84,11 +84,10 @@ class Auth:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """This updates the password for the user
-        """
+        """ Update password """
         if not reset_token:
             return None
-
+            
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             pwd = _hash_password(password)

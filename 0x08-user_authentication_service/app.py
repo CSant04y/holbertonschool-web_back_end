ac41180e0f4a2bfc166b74023970a74a5c8baab3
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-18 01:24:04 +0000

    added task

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -92,5 +92,19 @@ def get_reset_password_token():
         return jsonify({"email": email, "reset_token": token}), 200
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """updates password"""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    password = request.form.get('password')
+
+    try:
+        AUTH.update_password(reset_token, password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == '__main__':
     app.run()

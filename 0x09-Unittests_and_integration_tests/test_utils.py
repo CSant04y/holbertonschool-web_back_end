4e3337c0b92a5f4bcc4dcbf2a288fe21e520f505
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-22 19:45:18 +0000

    added import to get method from utils file

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 '''Thic has the test class to test NestedMap'''
 import unittest
-from nose.tools import assert_equal
-from parameterized import parameterized, parameterized_class
+from parameterized import parameterized
 from typing import Mapping, Sequence, Any
-access_nested_map = __import__('utils').access_nested_map
+from utils import access_nested_map
+
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """This tests the NestedMap method"""
-
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected):
+    def test_access_nested_map(self, nested_map, path, expected):
         """test nested map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)

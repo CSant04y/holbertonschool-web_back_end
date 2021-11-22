7bd079e4b00bc3edcb4b4c66593066a2e55ba126
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-22 22:00:57 +0000

    added task

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@ import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 from typing import Mapping, Sequence, Any
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -43,7 +43,29 @@ class TestGetJson(unittest.TestCase):
             def json(self):
                 '''mocks json method'''
                 return test_payload
-        
+
         with patch("requests.get") as MockedClass:
             MockedClass.return_value = Mocked()
             self.assertAlmostEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    '''This class test Memoize method'''
+
+    def test_memoize(self):
+        '''This tests the memeoize method'''
+        class TestClass:
+
+            def a_method(self):
+                '''Returns 42 which is supposed to act as returned'''
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method') as testClass:
+            class_obj = TestClass()
+            class_obj.a_property
+            class_obj.a_property
+
+            testClass.asset_called_once()

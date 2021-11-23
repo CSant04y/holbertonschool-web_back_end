5c9bdf157b9358ffc73a72bda419fab5482d10b3
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-23 19:57:48 +0000

    aded file

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
-"""module for testing client.py"""
+'''Ths module contians classes to test client.py methods'''
+import client
+from parameterized import parameterized
 import unittest
-from unittest.mock import patch, PropertyMock, Mock
-from parameterized import parameterized, parameterized_class
-from client import GithubOrgClient
-from fixtures import TEST_PAYLOAD
-from requests import Response
+from unittest import mock
+from unittest.mock import patch, Mock
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """tests GithubOrgClient functionality"""
+    '''This class tests GithubOrgClient method'''
+
     @parameterized.expand([
-        ('google'),
-        ('abc'),
+        ("google"),
+        ("abc")
     ])
-    @patch('client.get_json', return_value={'k': 'v'})
-    def test_org(self, org_name, mock_gj):
-        """tests that GithubOrgClient.org returns correct value"""
-        test = GithubOrgClient(org_name)
-        self.assertEqual(test.org, {'k': 'v'})
-        url = 'https://api.github.com/orgs/{}'.format(org_name)
-        mock_gj.assert_called_once_with(url)
+    @patch('client.get_json', return_value={'k': 'y'})
+    def test_org(self, url, my_mock):
+        """
+        Test GithubOrgClient.org
+        """
+        my_mock.return_value = True
+        g = client.GithubOrgClient(url)
+        self.assertEqual(g.org, True)
+        my_mock.assert_called_once()

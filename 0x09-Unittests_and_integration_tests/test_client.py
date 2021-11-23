ed0c5f920067e0cd31159693685896e6f248dad2
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-23 22:24:47 +0000

    added file

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 '''Ths module contians classes to test client.py methods'''
 import client
+from client import GithubOrgClient
 from parameterized import parameterized, parameterized_class
 import unittest
 from unittest import mock
-from unittest.mock import patch, Mock
+from unittest.mock import PropertyMock, patch, Mock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -23,3 +24,13 @@ class TestGithubOrgClient(unittest.TestCase):
         g = client.GithubOrgClient(url)
         self.assertEqual(g.org, True)
         my_mock.assert_called_once()
+
+    @parameterized.expand([
+        ('random-url', {'repos_url': 'http://some_url.com'})
+        ])
+    def test_public_repos_url(self, name, result):
+        '''This tests _puclic_repo'''
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value=result)):
+            respsonse = client.GithubOrgClient(name)._public_repos_url
+            self.assertEqual(respsonse, result.get('repos_url'))

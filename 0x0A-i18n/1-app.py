05eaf2726cd6d18e68fef3be8fb302e2653c6499
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-24 16:28:25 +0000

    added task

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -3,11 +3,10 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 app = Flask(__name__)
-
 babel = Babel(app)
 
 
-class Config:
+class Config(object):
     '''langauge configuration class'''
     LANGUAGES = ["en", "fr"]
 

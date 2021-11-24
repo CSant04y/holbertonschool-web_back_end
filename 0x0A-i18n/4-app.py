6d008dfde2be40c07655db24201694098fa12751
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-24 21:32:26 +0000

    addes to conditional statement

--- 0x0A-i18n/4-app.py
+++ 0x0A-i18n/4-app.py
@@ -26,7 +26,7 @@ def get_locale():
     """
     locale = request.args.get('locale')
 
-    if locale:
+    if locale and (locale == 'en' or locale == 'fr'):
         return locale
 
     return request.accept_languages.best_match(Config.LANGUAGES)

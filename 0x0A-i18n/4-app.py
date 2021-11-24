c673572d4fbba90a14de15ecfa1bc1cf49c4da65
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-24 21:05:00 +0000

    added file

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+'''This Is the flask application'''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """[This class sets the config for babel]s
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """[This determinds the best match in language]
+
+    Returns:
+        [Str]: [Lanaguage choosen]
+    """
+    locale = request.args.get('locale')
+
+    if locale:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """[This renders the template for website]
+
+    Returns:
+        [template]: rendered template
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")

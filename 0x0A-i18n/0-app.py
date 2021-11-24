5fe0d19bcf65a5b4be31b12bbb9436214ece4614
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-24 15:53:44 +0000

    added task

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -5,7 +5,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def render_template():
     '''This renders the index.html template'''
     return render_template('templates/0-index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")

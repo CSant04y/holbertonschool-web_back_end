fcbd38d05bf9fbecc4f59a8968614a1cbe882e5a
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-24 15:54:59 +0000

    added task

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -10,5 +10,6 @@ def render_template():
     '''This renders the index.html template'''
     return render_template('0-index.html')
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

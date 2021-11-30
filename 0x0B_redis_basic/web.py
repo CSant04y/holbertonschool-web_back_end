b43990fbb7f7c352a5d97ef23e5dc671cb2b1649
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-30 22:00:38 +0000

    added task

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+request to url
+"""
+import redis
+from typing import Callable
+import requests
+from functools import wraps
+
+
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
+
+    @wraps(fn)
+    def wrapper(url):
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
+
+    return wrapper
+
+
+@wrap_requests
+def get_page(url: str) -> str:
+    """gets page from url"""
+    response = requests.get(url)
+    return response.text

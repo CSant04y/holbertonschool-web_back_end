bdf7cb0aa0eeb68fbd0c4eee688e78c0baf4c447
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-30 21:49:46 +0000

    fixed typo

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -42,7 +42,7 @@ def replay(method: Callable):
     if stored_key:
         times = self_.get_str(stored_key)
         inputs = self_.redis.lrange(stored_name + ":inputs", 0, -1)
-        outputs = self_._redis.lrange(stored_name + ":outputs", 0, -1)
+        outputs = self_.redis.lrange(stored_name + ":outputs", 0, -1)
 
         print(f"{stored_name} was called {times} times:")
         zipvalues = zip(inputs, outputs)

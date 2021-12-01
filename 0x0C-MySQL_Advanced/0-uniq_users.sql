ab9818242f220f69c0acdcd72ef51a6f8bce5f2e
CSant04y <esquivelcarlos27893@gmail.com>
2021-12-01 18:56:58 +0000

    made script to make table called users

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,1 +1,7 @@
-
+-- Creates Table "users" with id, email, name
+-- gets unique
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255)
+); 

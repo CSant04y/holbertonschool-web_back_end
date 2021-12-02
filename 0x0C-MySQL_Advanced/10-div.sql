0bedde7a4844d3f820184d46b1bed97b697f61d7
CSant04y <esquivelcarlos27893@gmail.com>
2021-12-02 22:17:04 +0000

    added files

---
+++ 0x0C-MySQL_Advanced/10-div.sql
@@ -0,0 +1,11 @@
+-- This Script creates a function that divides and returns
+DELIMITER //
+
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+	RETURN (IF (b = 0, 0, a / b));
+END //
+
+DELIMITER ;

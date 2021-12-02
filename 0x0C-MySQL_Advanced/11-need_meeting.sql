fbb5b5f8fa75c0780e7e823e9efe80d612e695b1
CSant04y <esquivelcarlos27893@gmail.com>
2021-12-02 22:40:04 +0000

    added comments

--- 0x0C-MySQL_Advanced/11-need_meeting.sql
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -1,3 +1,4 @@
+-- Script that lists students that have score under 80
 CREATE VIEW need_meeting AS
 SELECT name FROM students WHERE score < 80
 AND (students.last_meeting IS NULL OR students.last_meeting < DATE_ADD(NOW(), INTERVAL -1 MONTH));

b935e94f75a3068763dfc9d502002bd75224daf7
CSant04y <esquivelcarlos27893@gmail.com>
2021-12-14 09:07:08 -0600

    changed handling of error in catch

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -2,7 +2,7 @@ function handleResponseFromAPI(promise) {
   const resp = { status: 200, body: 'Success' };
   return promise
     .then(() => resp)
-    .catch(() => Error())
+    .catch((error) => error)
     .finally(() => {
       console.log('Got a response from the API');
     });

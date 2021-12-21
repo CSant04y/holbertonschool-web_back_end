c3cc182485c3901dbc916b20d5839c75bc1cb588
CSant04y <esquivelcarlos27893@gmail.com>
2021-12-21 13:39:58 -0600

    added task 8

---
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+const cleanSet = (set, startString) => {
+  if (!startString || !startString.length) return '';
+  let value = '';
+
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      value += value.length === 0 ? item.replace(startString, '') : item.replace(startString, '-');
+    }
+  }
+  return value;
+};
+
+export default cleanSet;

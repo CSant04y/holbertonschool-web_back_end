f45fe841caedf6f0ed93d197f49b712a23318b1b
CSant04y <esquivelcarlos27893@gmail.com>
2022-01-02 12:42:25 -0600

    added task

---
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, (error, csvFileData) => {
+    if (error) {
+      reject(Error('Cannot load the database'));
+    }
+    if (csvFileData) {
+      let data;
+      const fields = {};
+
+      try {
+        data = fs.readFileSync(path);
+      } catch (err) {
+        throw new Error('Cannot load the database');
+      }
+      data = data.toString().split('\n');
+      data = data.filter((element) => element.length > 0);
+      data.shift();
+
+      data.forEach((element) => {
+        if (element.length > 0) {
+          const row = element.split(',');
+          if (row[3] in fields) {
+            fields[row[3]].push(row[0]);
+          } else {
+            fields[row[3]] = [row[0]];
+          }
+        }
+      });
+      console.log(`Number of students: ${data.length}`);
+      for (const field in fields) {
+        if (field) {
+          const list = fields[field];
+          console.log(`Number of students in ${field}: ${list.length}. List: ${list.toString().replace(/,/g, ', ')}`);
+        }
+      }
+    }
+    resolve();
+  });
+});
+
+module.exports = countStudents;

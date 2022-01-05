85d86cd2dc442335a4d9f5f6ce5f349aa7eee694
CSant04y <esquivelcarlos27893@gmail.com>
2022-01-05 11:17:43 -0600

    added task 1

---
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,22 @@
+const assert = require("assert");
+const calculateNumber = require("./1-calcul.js");
+
+describe("calculateNumber", function () {
+  it("Calculates SUM", function () {
+    assert.strictEqual(calculateNumber("SUM", 5.2, 1.8), 7);
+    assert.strictEqual(calculateNumber("SUM", 5.21, 2.89), 8);
+    assert.strictEqual(calculateNumber("SUM", 7.1, 2.5), 10);
+  })
+  it("Calculates SUBTRACTION", function () {
+    assert.strictEqual(calculateNumber("SUBTRACT", -2.5, -6.2), -8);
+    assert.strictEqual(calculateNumber("SUBTRACT", -50.1, -10.8), -61);
+    assert.strictEqual(calculateNumber("SUBTRACT", 3.85, -10.2), -6);
+    assert.strictEqual(calculateNumber("SUBTRACT", -10, 10), 0);
+  });
+
+  it("Calculates Division", function() {
+    assert.strictEqual(calculateNumber("DIVIDE", 0, 0), 0);
+    assert.strictEqual(calculateNumber("DIVIDE", 100.6736, 0), 10.1);
+    assert.strictEqual(calculateNumber("DIVIDE", 0, 50.1234), 50);
+  });
+});

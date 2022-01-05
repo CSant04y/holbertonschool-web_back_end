48ce43d6447a3149fd59845e2793f49d736fba1f
CSant04y <esquivelcarlos27893@gmail.com>
2022-01-05 13:49:35 -0600

    added task chnage

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -9,19 +9,31 @@ describe("calculateNumber type=SUM", function () {
     expect(calculateNumber("SUM", 5.21, 2.89)).to.equal(8);
     expect(calculateNumber("SUM", 7.1, 2.5)).to.equal(10);
   });
+});
 
+describe("calculateNumber", () => {
   it("Calculates SUBTRACT", function () {
     expect(calculateNumber("SUBTRACT", -2.5, -6.2)).to.equal(4);
     expect(calculateNumber("SUBTRACT", -50.1, -10.8)).to.equal(-39);
     expect(calculateNumber("SUBTRACT", 3.85, -10.2)).to.equal(14);
     expect(calculateNumber("SUBTRACT", -10, 10)).to.equal(-20);
   });
+});
 
+describe("calculateNumber", () => {
+  it("Calculates SUBTRACT", function () {
+    expect(calculateNumber("SUBTRACT", -2.5, -6.2)).to.equal(4);
+    expect(calculateNumber("SUBTRACT", -50.1, -10.8)).to.equal(-39);
+    expect(calculateNumber("SUBTRACT", 3.85, -10.2)).to.equal(14);
+    expect(calculateNumber("SUBTRACT", -10, 10)).to.equal(-20);
+  });
+});
+
+describe("calculateNumber", () => {
   it("Calculates DIVIDE", function() {
     expect(calculateNumber("DIVIDE", 0, 0)).to.equal("Error");
     expect(calculateNumber("DIVIDE", 100.6736, 0)).to.equal("Error");
     expect(calculateNumber("DIVIDE", 0, 50.1234)).to.equal(0);
     expect(calculateNumber("DIVIDE", 10, 5)).to.equal(2);
   });
-
 });

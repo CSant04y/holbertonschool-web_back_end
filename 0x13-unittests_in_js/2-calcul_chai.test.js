d8f7f7c84d332f89f6e206b9617da7f5e1c5aee4
CSant04y <esquivelcarlos27893@gmail.com>
2022-01-05 13:40:37 -0600

    added string to test for test

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const { type } = require("chai/lib/chai/utils");
 const calculateNumber = require("./1-calcul.js");
 
-describe("calculateNumber", function () {
+describe("calculateNumber SUM", function () {
 
   it("Calculates SUM", function () {
     expect(calculateNumber("SUM", 5.2, 1.8)).to.equal(7);
@@ -10,14 +10,14 @@ describe("calculateNumber", function () {
     expect(calculateNumber("SUM", 7.1, 2.5)).to.equal(10);
   });
 
-  it("Calculates SUBTRACTION", function () {
+  it("Calculates SUBTRACT", function () {
     expect(calculateNumber("SUBTRACT", -2.5, -6.2)).to.equal(4);
     expect(calculateNumber("SUBTRACT", -50.1, -10.8)).to.equal(-39);
     expect(calculateNumber("SUBTRACT", 3.85, -10.2)).to.equal(14);
     expect(calculateNumber("SUBTRACT", -10, 10)).to.equal(-20);
   });
 
-  it("Calculates Division", function() {
+  it("Calculates DIVIDE", function() {
     expect(calculateNumber("DIVIDE", 0, 0)).to.equal("Error");
     expect(calculateNumber("DIVIDE", 100.6736, 0)).to.equal("Error");
     expect(calculateNumber("DIVIDE", 0, 50.1234)).to.equal(0);

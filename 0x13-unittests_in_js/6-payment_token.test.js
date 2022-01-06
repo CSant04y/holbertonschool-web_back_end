0c144bfabef9d033c8c9c097482b33d5f9eae150
CSant04y <esquivelcarlos27893@gmail.com>
2022-01-05 21:21:22 -0600

    added task

--- 0x13-unittests_in_js/6-payment_token.test.js
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -1,12 +1,12 @@
 const expect = require('chai').expect;
-const getPaymentTokenFromAPI = require('./6-payment_token-0');
+const getPaymentTokenFromAPI = require('./6-payment_token.js');
 
-describe("getPaymentTokenFromAPI function", () => {
-  it("testing async function with promise", (done) => {
+describe('getPaymentTokenFromAPI function', () => {
+  it('async testing Promise', (done) => {
     getPaymentTokenFromAPI(true)
       .then((res) => {
-          expect(res).to.eql({ data: 'Successful response from the API' })
-      });
+        expect(res).to.eql({ data: 'Successful response from the API' })
+      })
       done();
-  });
+  })
 })

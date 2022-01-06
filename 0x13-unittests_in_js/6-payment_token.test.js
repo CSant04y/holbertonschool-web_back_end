121167f96181dc5f609beb989a118f0e91ab93cd
CSant04y <esquivelcarlos27893@gmail.com>
2022-01-05 21:20:02 -0600

    added chnages to format

--- 0x13-unittests_in_js/6-payment_token.test.js
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -1,10 +1,12 @@
 const expect = require('chai').expect;
-const getPaymentTokenFromAPI = require('./6-payment_token.js');
+const getPaymentTokenFromAPI = require('./6-payment_token-0');
 
 describe("getPaymentTokenFromAPI function", () => {
-    it("testing async function", (done) => {
-        getPaymentTokenFromAPI(true)
-            .then((res) => expect(res).to.eql({ data: 'Successful response from the API' }));
-            done();
-    });
+  it("testing async function with promise", (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+          expect(res).to.eql({ data: 'Successful response from the API' })
+      });
+      done();
+  });
 })

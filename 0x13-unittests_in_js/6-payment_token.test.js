d5c5a3185280e00b333d9c5dcd0d09f87ca8a6e9
CSant04y <esquivelcarlos27893@gmail.com>
2022-01-05 20:38:29 -0600

    added more destiptors

--- 0x13-unittests_in_js/6-payment_token.test.js
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 const getPaymentTokenFromAPI = require('./6-payment_token.js');
 
-describe("getPaymentTokenFromAPI", () => {
+describe("getPaymentTokenFromAPI function", () => {
     it("testing async function", (done) => {
         getPaymentTokenFromAPI(true)
             .then((res) => expect(res).to.eql({ data: 'Successful response from the API' }));
